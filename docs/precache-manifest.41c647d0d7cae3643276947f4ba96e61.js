573ee1d7daefde28412b932404273313b7396acd
jovaslink <jovaslink@gmail.com>
2020-09-24 18:52:35 -0500

    Agregamos Docs

---
+++ docs/precache-manifest.41c647d0d7cae3643276947f4ba96e61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc982c52781d178bba686b8aaa344d98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f2eb83a3454c6469f06",
+    "url": "/static/css/main.3935ab09.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f2eb83a3454c6469f06",
+    "url": "/static/js/main.dd9f1337.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
